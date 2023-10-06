2e2c6101f56f41cbc31105ea7eff501ef8dcd8d2
Mina <minamaurice810@gmail.com>
2023-10-07 00:24:43 +0300

    Make the code more beautiful and better to read

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,17 @@
 */
 int main(void)
 {
-char c;
-int i;
-long a;
-long long z;
-float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	char c;
+	int i;
+	long a;
+	long long z;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
 }
